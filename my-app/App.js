00a1e12c677ea50c10131bd156cb94f9ca1abaac
virti7 <virtipanchamia25@gmail.com>
2025-08-29 19:12:25 +0530

    Initial commit: add my-app React Native project

---
+++ my-app/App.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts, DancingScript_400Regular } from "@expo-google-fonts/dancing-script";
+import { Lobster_400Regular } from "@expo-google-fonts/lobster";
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello, welcome to my first page!</Text>
+      <View style={{ marginTop: 20 }}>
+        <Button title="Navigate" onPress={() =>
+           navigation.navigate("Second")} />
+      </View>
+    </View>
+  );
+}
+
+function SecondScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.subtitle}>Welcome Virti!!</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    DancingScript_400Regular,
+    Lobster_400Regular
+  });
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Second" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, justifyContent: "center", alignItems: "center"
+  },
+  title: { 
+    fontFamily: "DancingScript_400Regular", 
+    fontSize: 32, 
+    color: "#240e79ff",
+    textAlign: "center"
+  },
+  subtitle: { 
+    fontFamily: "Lobster_400Regular", 
+    fontSize: 24, 
+    color: "#264794ff", 
+    marginVertical: 12
+  },
+});
